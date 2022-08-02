e7f5ebdbd54016f508e2745046b50e27904135e3
Frazonar <snip.coloured@gmail.com>
2022-08-02 21:01:36 +0300

    refactor: move preloaded image link to Portal

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,4 +1,5 @@
-import { Component, onMount } from "solid-js";
+import { Component } from "solid-js";
+import { Portal } from "solid-js/web";
 
 import "./Intro.scss";
 
@@ -6,32 +7,28 @@ import avatarImg from "../images/avatar.webp";
 import downIcon from "../images/icons/icon-downArrow.svg";
 
 const Intro: Component = () => {
-  onMount(() => {
-    const link = document.createElement("link");
-    link.rel = "preload";
-    link.as = "image";
-    link.href = avatarImg;
-    link.type = "image/webp";
-    document.head.appendChild(link);
-  });
-
   return (
-    <header class="Intro" id="home">
-      <div class="Intro__container">
-        <h1 class="Intro__title">Hi, I’m Elhanan.</h1>
-        <p class="Intro__subtitle">
-          I’m a full-stack developer. Programming is my passion.
-        </p>
-        <img class="Intro__avatar" src={avatarImg} alt="Owner avatar" />
-      </div>
-      <a class="Intro__down-link" href="#about-me">
-        <img
-          class="Intro__down-icon"
-          src={downIcon}
-          alt="Down pointing arrow"
-        />
-      </a>
-    </header>
+    <>
+      <Portal mount={document.head}>
+        <link rel="preload" as="image" href={avatarImg} type="image/webp" />
+      </Portal>
+      <header class="Intro" id="home">
+        <div class="Intro__container">
+          <h1 class="Intro__title">Hi, I’m Elhanan.</h1>
+          <p class="Intro__subtitle">
+            I’m a full-stack developer. Programming is my passion.
+          </p>
+          <img class="Intro__avatar" src={avatarImg} alt="Owner avatar" />
+        </div>
+        <a class="Intro__down-link" href="#about-me">
+          <img
+            class="Intro__down-icon"
+            src={downIcon}
+            alt="Down pointing arrow"
+          />
+        </a>
+      </header>
+    </>
   );
 };
 
