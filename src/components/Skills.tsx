30b11f1213f4733e89bf978cb814cdebc2930def
synthetic-borealis <snip.coloured@gmail.com>
2022-09-12 14:27:47 +0300

    Refactor & format

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,17 @@
-import { Component, children as Children, JSXElement } from 'solid-js';
+import { Component, JSXElement } from 'solid-js';
 
 import './Skills.scss';
 
 interface ISkillsProps {
-  children: JSXElement;
+  children: JSXElement | JSXElement[];
 }
 
 const Skills: Component<ISkillsProps> = (props: ISkillsProps) => {
-  const children = Children(() => props.children);
-
   return (
     <section class="Skills" id="skills">
       <div class="Skills__container">
         <h2 class="Skills__title">Skills</h2>
-        <ul class="Skills__lists-container">{children()}</ul>
+        <ul class="Skills__lists-container">{props.children}</ul>
       </div>
     </section>
   );
