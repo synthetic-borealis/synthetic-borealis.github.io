109e57dbdd27121cf1510781a849ab99f98c5d7c
synthetic-borealis <snip.coloured@gmail.com>
2025-01-19 20:38:12 +0200

    Refactor i18n configuration

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,13 +3,17 @@ import { initReactI18next } from 'react-i18next';
 
 import en from './en.json' with { type: 'json' };
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: en,
+function configureI18n(lng: string) {
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: en,
+      },
     },
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-  defaultNS: 'translation',
-});
+    lng,
+    fallbackLng: 'en',
+    defaultNS: 'translation',
+  });
+}
+
+export default { configureI18n };
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
 
-import './i18n/index.ts';
+import i18n from './i18n/index.ts';
+
+i18n.configureI18n('en');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
