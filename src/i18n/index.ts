1ebcb4e2844c4e683853e330fa8ecb8ae274efa2
synthetic-borealis <snip.coloured@gmail.com>
2025-01-19 20:15:08 +0200

    Add react-i18next

---
+++ src/i18n/index.ts
@@ -0,0 +1,8 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  resources: {},
+  lng: 'en',
+  fallbackLng: 'en',
+});
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
 
+import './i18n/index.ts';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
