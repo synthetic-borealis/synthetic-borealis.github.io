64b978a0bebf3263e5aea2b29d66814c6d9c00bd
synthetic-borealis <snip.coloured@gmail.com>
2025-01-26 18:21:03 +0200

    remove test logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './App.scss';
 
-import { useAppSelector, useAppDispatch } from './app/hooks.ts';
-import { setLanguage } from './features/language/languageSlice.ts';
-
 function App() {
-  const dispatch = useAppDispatch();
-  const language = useAppSelector((state) => state.language.value);
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    const changeLanguage = async () => {
-      await i18n.changeLanguage(language);
-    };
-    changeLanguage().catch(() => {});
-  }, [language, i18n]);
+  const { t } = useTranslation();
 
   return (
     <>
       <h1>{t('app.placeholderWelcome')}</h1>
-      <button onClick={() => dispatch(setLanguage(language === 'en' ? 'he' : 'en'))}>{t('app.changeLanguage')}</button>
     </>
   );
 }
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,16 +2,12 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './en.json' with { type: 'json' };
-import he from './he.json' with { type: 'json' };
 
 function configureI18n(lng: string) {
   return i18n.use(initReactI18next).init({
     resources: {
       en: {
         translation: en,
-      },
-      he: {
-        translation: he,
       },
     },
     lng,
